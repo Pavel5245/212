0f2b763ddd75fd956d75d5b42a1d26983dcccbea
Pavel <Pavel.e.belov2010@gmail.com>
2025-06-30 11:14:44 +0300

    example_wirehouse

--- 212.py
+++ 212.py
@@ -1,21 +1,19 @@
 import pandas as pd
 import psycopg2 as psy
 import pprint as pp
-df = pd.read_html('C:/Users/Pavel/Documents/Partners.htm', index_col=0)
+df = pd.read_html('C:/Users/Павел/Documents/Python/Info/Поступления/Склады.htm', index_col=0)
 conn = psy.connect(dbname='WhProd', user='postgres', password='adminPavel', host='localhost', port=5432)
 n = 0
+printer = pp.PrettyPrinter()
 cur = conn.cursor()
 for row in df:
-            
-            val = row[3].to_string(header=False, index=False, na_rep='Not')
-            # n += 1
-            print(val)
-            cur.execute(
-                'INSERT INTO inv_partner ("Name") VALUES (%s)',
-                (val)  
-                )
 
+    for index in range(len(row)):
+        # printer.pprint(object=row[1].iloc[index])
+        cur.execute(
+            'INSERT INTO inv_wirehouse ("number_1C", "name") VALUES (%s, %s)',
+                (row[1].iloc[index], row[2].iloc[index])
+        )
 conn.commit()
-cur.close()            
-
-conn.close()  
+cur.close()
+conn.close()
