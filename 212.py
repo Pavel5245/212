153c81cf974bed6307c9384e877e5eeffa4634bf
Pavel <Pavel.e.belov2010@gmail.com>
2025-07-01 19:27:08 +0300

    new

--- 212.py
+++ 212.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import psycopg2 as psy
 import pprint as pp
-df = pd.read_html('C:/Users/Павел/Documents/Python/Info/Поступления/Склады.htm', index_col=0)
+import string as s
+
+df = pd.read_html('C:/Users/Павел/Documents/Python/Info/Контрагенты/Контрагенты.htm', index_col=0, skiprows=1, encoding='utf-8')
 conn = psy.connect(dbname='WhProd', user='postgres', password='adminPavel', host='localhost', port=5432)
 n = 0
 printer = pp.PrettyPrinter()
@@ -9,10 +11,18 @@ cur = conn.cursor()
 for row in df:
 
     for index in range(len(row)):
-        # printer.pprint(object=row[1].iloc[index])
+
+        name = str(row[3].iloc[index]).replace("\xa0", ' ')
+        inn = str(row[4].iloc[index]).replace("\xa0", ' ')
+        manager = str(row[6].iloc[index]).replace("\xa0", ' ')
+
+        # printer.pprint(name)
+        # n += 1
+        # if n == 10:
+        #     break
         cur.execute(
-            'INSERT INTO inv_wirehouse ("number_1C", "name") VALUES (%s, %s)',
-                (row[1].iloc[index], row[2].iloc[index])
+            'INSERT INTO inv_partner (name, "INN", manager) VALUES (%s, %s, %s)',
+                (name, inn, manager)
         )
 conn.commit()
 cur.close()
